6fffdbb2add945b698e830553cced12464c17607
broderick99 <westono@Wess-MacBook-Air.local>
2014-07-30 12:41:50 -0600

    Initial Commit

---
+++ testerTests/testerTests.m
@@ -0,0 +1,34 @@
+//
+//  testerTests.m
+//  testerTests
+//
+//  Created by Lisa Ostler on 7/30/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testerTests : XCTestCase
+
+@end
+
+@implementation testerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
